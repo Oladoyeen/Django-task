5d205f2b211728a416ee00f849e61508b09efad5
Oladoyeen <81159910+Oladoyeen@users.noreply.github.com>
2021-04-27 23:27:42 +0100

    codes were updated

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,13 @@ class BlogListView(ListView):
     template_name = 'home.html'
 
 
-    class BlogListView(DetailView):
-        model = Post
-        template_name = 'post_detail.html'
+class BlogListView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+
+class BlogCreateView(CreateView):
+    model=Post
+    template_name = 'post_new'
+    fields = ['title', 'author', 'body']
         
